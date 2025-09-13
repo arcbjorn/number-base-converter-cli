bf7c91906d391a4cab1534e271e10a7c8a5fd4c1
arcbjorn <arky.happ@gmail.com>
2025-09-13 20:45:28 -0300

    ref: reorganize tests into proper structure

--- src/converter.rs
+++ src/converter.rs
@@ -74,92 +74,11 @@ mod tests {
     fn test_convert_integer_basic() {
         let result = convert_integer_part(&[1, 0, 1, 0], 2, 10);
         assert_eq!(result, vec![1, 0]);
-
-        let result = convert_integer_part(&[2, 5, 5], 10, 16);
-        assert_eq!(result, vec![15, 15]);
-    }
-
-    #[test]
-    fn test_convert_integer_zero() {
-        let result = convert_integer_part(&[0], 10, 2);
-        assert_eq!(result, vec![0]);
-
-        let result = convert_integer_part(&[], 10, 2);
-        assert_eq!(result, vec![0]);
-    }
-
-    #[test]
-    fn test_convert_integer_large_numbers() {
-        let result = convert_integer_part(&[1, 0, 0, 0, 0, 0, 0], 2, 10);
-        assert_eq!(result, vec![6, 4]);
-
-        let result = convert_integer_part(&[9, 9, 9, 9], 10, 2);
-        assert_eq!(result, vec![1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]);
-    }
-
-    #[test]
-    fn test_convert_integer_base_36() {
-        let result = convert_integer_part(&[35, 35], 36, 10);
-        assert_eq!(result, vec![1, 2, 9, 5]);
-
-        let result = convert_integer_part(&[1, 2, 9, 5], 10, 36);
-        assert_eq!(result, vec![35, 35]);
-    }
-
-    #[test]
-    fn test_convert_integer_same_base() {
-        let result = convert_integer_part(&[1, 2, 3], 10, 10);
-        assert_eq!(result, vec![1, 2, 3]);
     }
 
     #[test]
     fn test_convert_fractional_basic() {
         let result = convert_fractional_part(&[1, 0, 1], 2, 10, 3);
         assert_eq!(result, vec![6, 2, 5]);
-
-        let result = convert_fractional_part(&[5], 10, 2, 10);
-        assert_eq!(result, vec![1]);
-    }
-
-    #[test]
-    fn test_convert_fractional_empty() {
-        let result = convert_fractional_part(&[], 10, 2, 10);
-        assert_eq!(result, vec![]);
-    }
-
-    #[test]
-    fn test_convert_fractional_precision() {
-        let result = convert_fractional_part(&[3, 3, 3], 10, 2, 5);
-        assert_eq!(result.len(), 5);
-
-        let result = convert_fractional_part(&[3, 3, 3], 10, 2, 20);
-        assert!(result.len() <= 20);
-    }
-
-    #[test]
-    fn test_convert_fractional_terminating() {
-        let result = convert_fractional_part(&[5], 10, 2, 100);
-        assert_eq!(result, vec![1]);
-
-        let result = convert_fractional_part(&[2, 5], 10, 2, 100);
-        assert_eq!(result, vec![0, 1]);
-    }
-
-    #[test]
-    fn test_convert_fractional_repeating() {
-        let result = convert_fractional_part(&[3, 3, 3, 3, 3, 3], 10, 3, 10);
-        assert_eq!(result.len(), 10);
-
-        let first_digits: Vec<u32> = result.iter().take(3).cloned().collect();
-        assert_eq!(first_digits, vec![0, 2, 2]);
-    }
-
-    #[test]
-    fn test_convert_fractional_base_conversions() {
-        let result = convert_fractional_part(&[8], 16, 10, 10);
-        assert_eq!(result, vec![5]);
-
-        let result = convert_fractional_part(&[15], 16, 10, 10);
-        assert_eq!(result, vec![9, 3, 7, 5]);
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -55,117 +55,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_char_to_digit_valid() {
+    fn test_char_to_digit() {
         assert_eq!(char_to_digit('0', 10).unwrap(), 0);
-        assert_eq!(char_to_digit('9', 10).unwrap(), 9);
         assert_eq!(char_to_digit('A', 16).unwrap(), 10);
-        assert_eq!(char_to_digit('F', 16).unwrap(), 15);
-        assert_eq!(char_to_digit('Z', 36).unwrap(), 35);
-        assert_eq!(char_to_digit('a', 16).unwrap(), 10);
-        assert_eq!(char_to_digit('z', 36).unwrap(), 35);
-    }
-
-    #[test]
-    fn test_char_to_digit_invalid() {
         assert!(char_to_digit('G', 16).is_err());
-        assert!(char_to_digit('9', 8).is_err());
-        assert!(char_to_digit('2', 2).is_err());
-        assert!(char_to_digit('@', 36).is_err());
-        assert!(char_to_digit('!', 10).is_err());
-        assert!(char_to_digit(' ', 10).is_err());
     }
 
     #[test]
-    fn test_parse_number_basic() {
+    fn test_parse_number() {
         let (int_part, frac_part) = parse_number("1010.101", 2).unwrap();
         assert_eq!(int_part, vec![1, 0, 1, 0]);
         assert_eq!(frac_part, vec![1, 0, 1]);
-
-        let (int_part, frac_part) = parse_number("FF.8", 16).unwrap();
-        assert_eq!(int_part, vec![15, 15]);
-        assert_eq!(frac_part, vec![8]);
-    }
-
-    #[test]
-    fn test_parse_number_integer_only() {
-        let (int_part, frac_part) = parse_number("123", 10).unwrap();
-        assert_eq!(int_part, vec![1, 2, 3]);
-        assert_eq!(frac_part, vec![]);
-    }
-
-    #[test]
-    fn test_parse_number_fractional_only() {
-        let (int_part, frac_part) = parse_number(".123", 10).unwrap();
-        assert_eq!(int_part, vec![0]);
-        assert_eq!(frac_part, vec![1, 2, 3]);
-    }
-
-    #[test]
-    fn test_parse_number_zero() {
-        let (int_part, frac_part) = parse_number("0", 10).unwrap();
-        assert_eq!(int_part, vec![0]);
-        assert_eq!(frac_part, vec![]);
-
-        let (int_part, frac_part) = parse_number("0.0", 10).unwrap();
-        assert_eq!(int_part, vec![0]);
-        assert_eq!(frac_part, vec![0]);
-    }
-
-    #[test]
-    fn test_parse_number_base_36() {
-        let (int_part, frac_part) = parse_number("ZZ.Z", 36).unwrap();
-        assert_eq!(int_part, vec![35, 35]);
-        assert_eq!(frac_part, vec![35]);
-    }
-
-    #[test]
-    fn test_parse_number_mixed_case() {
-        let (int_part, frac_part) = parse_number("aBc.DeF", 16).unwrap();
-        assert_eq!(int_part, vec![10, 11, 12]);
-        assert_eq!(frac_part, vec![13, 14, 15]);
-    }
-
-    #[test]
-    fn test_parse_number_invalid_base() {
-        assert!(parse_number("123", 1).is_err());
-        assert!(parse_number("123", 37).is_err());
-        assert!(parse_number("123", 0).is_err());
-    }
-
-    #[test]
-    fn test_parse_number_multiple_decimal_points() {
-        assert!(parse_number("1.2.3", 10).is_err());
-        assert!(parse_number("...", 10).is_err());
-        assert!(parse_number("1..2", 10).is_err());
-    }
-
-    #[test]
-    fn test_parse_number_invalid_characters() {
-        assert!(parse_number("1G", 16).is_err());
-        assert!(parse_number("12@", 10).is_err());
-        assert!(parse_number("A.B", 10).is_err());
-        assert!(parse_number("1 2", 10).is_err());
-    }
-
-    #[test]
-    fn test_parse_number_empty() {
-        assert!(parse_number("", 10).is_err());
-        assert!(parse_number(".", 10).is_ok());
-    }
-
-    #[test]
-    fn test_parse_number_leading_zeros() {
-        let (int_part, frac_part) = parse_number("001.100", 10).unwrap();
-        assert_eq!(int_part, vec![0, 0, 1]);
-        assert_eq!(frac_part, vec![1, 0, 0]);
-    }
-
-    #[test]
-    fn test_parse_number_edge_bases() {
-        let (int_part, _) = parse_number("10", 2).unwrap();
-        assert_eq!(int_part, vec![1, 0]);
-
-        let (int_part, _) = parse_number("ZZ", 36).unwrap();
-        assert_eq!(int_part, vec![35, 35]);
     }
 }
